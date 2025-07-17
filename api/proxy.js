cdab5dcc779d4bdbd15f851f3559d84316b025d5
p0c666 <3754484620@qq.com>
2025-07-17 08:18:49 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,5 +1,11 @@
 export default function handler(req, res) {
+  // 强制覆盖所有可能被代理修改的头
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Content-Type", "text/javascript");
+  res.setHeader("X-Content-Type-Options", "nosniff"); // 防MIME类型嗅探攻击
+
+  // 阻止代理缓存敏感响应
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate");
+
   res.status(200).send("alert(/poc/);");
 }
